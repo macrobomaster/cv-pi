cb920555e66781be718391ba93df3b073ab8c768
onepoundlion <onepoundlion@users.github.com>
2019-03-20 15:21:58 -0400

    update light detect

---
+++ LightDetect/LightDetect.h
@@ -0,0 +1,21 @@
+#ifndef LIGHTDETECT_H_INCLUDED
+#define LIGHTDETECT_H_INCLUDED
+
+#include <stdio.h>
+#include "opencv2/opencv.hpp"
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include <vector>
+
+using namespace cv;
+
+cv::Mat toHSV(cv::Mat InputMat);
+
+cv::Mat FilterColor(cv::Mat InputMat, int enemycolor);
+
+cv::Mat erode_dilate(cv::Mat InputMat);
+
+double angle(Point pt1, Point pt2, Point pt0);
+
+cv::Mat findRec(cv::Mat InputMat);
+#endif // LIGHTDETECT_H_INCLUDED
