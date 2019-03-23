45690cff61ad28e9e196eef36d3eaa3121aca8c8
onepoundlion <onepoundlion@users.github.com>
2019-03-22 21:28:20 -0400

    Updated HSVGUI

--- HSVGUI/main.cpp
+++ HSVGUI/main.cpp
@@ -2,27 +2,45 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
-
+#include <unistd.h>
+#include <thread>
 using namespace std;
 using namespace cv;
-
+int ix;
+int iy;
 Mat src;
 
-void HSVGUI( int event, int x, int y, int f, void *param){
-    //cv::VideoCapture cap(0);
-    //cap>>src;
-    if (event == EVENT_MOUSEMOVE) {
-        cv::VideoCapture cap(0);
-        if(!cap.isOpened())
-            std::cout<<"check camera";
-        namedWindow("HSV GUI", WINDOW_AUTOSIZE);
-        // Change exposure accordingly
-        int exposure=-7;
-        cap.set(CV_CAP_PROP_EXPOSURE, exposure);
-        cap>>src;
+
+void call_back(int event,int x,int y,int flag, void* param){  //SetMouseCallBack Function
+    if(event==EVENT_MOUSEMOVE){                               //Stores mouse position in globals on movement
+    ix = x;
+    iy = y;
+    }
+}
+
+
+void cam_up()
+{
+       Mat frame; //create frame of images
+
+       VideoCapture capture(0); //constructor for capture
+       namedWindow("HSV GUI", WINDOW_AUTOSIZE); //display window
+       for (;;)
+    {
+        setMouseCallback("HSV GUI",call_back); //mouse position update
+        // wait for a new frame from camera and store it into 'frame'
+        capture.read(frame); //read captured frame
+        // check if we succeeded
+        if (frame.empty()) {
+            cerr << "ERROR! blank frame grabbed\n";
+            break;
+        }
+        int exposure=0;
+        capture.set(CV_CAP_PROP_EXPOSURE, exposure);
+        capture>>src;
         Mat image=src.clone();
         Mat HSV;
-        Mat RGB=image(Rect(x,y,1,1));
+        Mat RGB=frame(Rect(ix,iy,1,1));
         cvtColor(RGB, HSV,CV_BGR2HSV);
 
         Vec3b hsv=HSV.at<Vec3b>(0,0);
@@ -32,34 +50,27 @@ void HSVGUI( int event, int x, int y, int f, void *param){
 
         char name[30];
         sprintf(name,"H=%d",H);
-        putText(image,name, Point(25,40) , FONT_HERSHEY_SIMPLEX, .7, Scalar(0,255,0), 2,8,false );
+        putText(frame,name, Point(25,40) , FONT_HERSHEY_SIMPLEX, .7, Scalar(0,255,0), 2,8,false );
 
         sprintf(name,"S=%d",S);
-        putText(image,name, Point(25,80) , FONT_HERSHEY_SIMPLEX, .7, Scalar(0,255,0), 2,8,false );
+        putText(frame,name, Point(25,80) , FONT_HERSHEY_SIMPLEX, .7, Scalar(0,255,0), 2,8,false );
 
         sprintf(name,"V=%d",V);
-        putText(image,name, Point(25,120) , FONT_HERSHEY_SIMPLEX, .7, Scalar(0,255,0), 2,8,false );
+        putText(frame,name, Point(25,120) , FONT_HERSHEY_SIMPLEX, .7, Scalar(0,255,0), 2,8,false );
 
-        sprintf(name,"X=%d",x);
-        putText(image,name, Point(25,300) , FONT_HERSHEY_SIMPLEX, .7, Scalar(0,0,255), 2,8,false );
+        sprintf(name,"X=%d",ix);
+        putText(frame,name, Point(25,300) , FONT_HERSHEY_SIMPLEX, .7, Scalar(0,0,255), 2,8,false );
 
-        sprintf(name,"Y=%d",y);
-        putText(image,name, Point(25,340) , FONT_HERSHEY_SIMPLEX, .7, Scalar(0,0,255), 2,8,false );
+        sprintf(name,"Y=%d",iy);
+        putText(frame,name, Point(25,340) , FONT_HERSHEY_SIMPLEX, .7, Scalar(0,0,255), 2,8,false );
 
- //imwrite("hsv.jpg",image);
-        cv::imshow( "HSV GUI", image );
+        // show live and wait for a key with timeout long enough to show images
+        imshow("HSV GUI", frame); //display modified frame
+        if (waitKey(5) >= 0)
+            break;
     }
-    else if(event != EVENT_MOUSEMOVE){
-        cv::VideoCapture cap(0);
-        if(!cap.isOpened())
-            std::cout<<"check camera";
-        namedWindow("HSV GUI", WINDOW_AUTOSIZE);
-        cap>>src;
-        cv::imshow( "HSV GUI", src );
 
-    }
 }
-
 int main()
 {
     /*
@@ -67,14 +78,22 @@ int main()
     if(!cap.isOpened())
         std::cout<<"check camera";
     */
-    namedWindow("HSV GUI", WINDOW_AUTOSIZE);
-
+    //Mat frame;
+    //namedWindow("HSV GUI", WINDOW_AUTOSIZE);
+    //thread t(mouse_up);
+    cam_up();
+    //t.join();
+    //mouse_up();
+ /*
     for (;;) {
         // cap>>src;
         setMouseCallback( "HSV GUI", HSVGUI);
+
         char c = cvWaitKey(50);
         if (c == 27)
             break;
 	}
+    */
+
 	return 0;
 }
